c2fcbc7bf26b859c939bce61688ebd91ed0bdec7
omartarek198 <87566788+omartarek198@users.noreply.github.com>
2021-11-15 18:31:59 +0200

    Add files via upload
    skeleton  code for donor  class

---
+++ donor.php
@@ -0,0 +1,23 @@
+<?php
+    include_once "iLogin.php";
+class Donor implements iLogin {
+
+    function __construct() {
+   
+    }
+    }
+       function Validate()
+   {
+        
+   }
+
+    function getInvoice($donInvoice)
+    {
+       
+    }
+
+
+
+
+
+?>
