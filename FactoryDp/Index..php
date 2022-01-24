2139cf5e1caca8265828b8b761f7b5006117fb52
omartarek198 <87566788+omartarek198@users.noreply.github.com>
2022-01-24 19:03:38 +0200

    Add files via upload

---
+++ FactoryDp/Index..php
@@ -0,0 +1,16 @@
+<?php
+
+include_once('AidFactory.php');
+
+
+   $aidfactory;
+    $aid;    
+    $temp = $_POST["radiobtn"];
+    
+ $aidfactory = new AidFactory();
+                       
+                $aid = $aidfactory->GetAidType($temp);
+        $aid->RequestAid();  
+        $aid->Display();
+    
+      
