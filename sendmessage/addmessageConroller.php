94df5ace4362beb7dd1344f84edd96e73a0b251b
MohamedAlaa3 <93541421+MohamedAlaa3@users.noreply.github.com>
2022-01-25 04:39:22 +0200

    send mrssage

---
+++ sendmessage/addmessageConroller.php
@@ -0,0 +1,15 @@
+<?php
+include_once 'addmessagemodel.php';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+echo 'Thank for addmessage2 <br>';
+
+$d= new addmessagemodel();
+echo $_POST["m"];
+//$d->messageid=$_POST["m"];
+$d->addtoDb($_POST["u"], $_POST["m"]);
+
+
